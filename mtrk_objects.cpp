834c35d9967efff7bae338c9e294d5b6df1f656a
Tobias Block <tobias.block@nyumc.org>
2020-02-02 19:07:32 -0500

    Continued.

--- mtrk_objects.cpp
+++ mtrk_objects.cpp
@@ -9,6 +9,7 @@ mtrk_objects::mtrk_objects()
 {
     objects=0;
     count=0;    
+    eventObjects=0;
 }
 
 
@@ -20,15 +21,111 @@ mtrk_objects::~mtrk_objects()
 
 void mtrk_objects::clear()
 {
-    // TODO
+    if (eventObjects!=0)
+    {
+        for (int i=0; i<count; i++)
+        {
+            if (eventObjects[i]!=0)
+            {
+                delete eventObjects[i];
+                eventObjects[i]=0;
+            }
+        }
+
+        delete[] eventObjects;
+        eventObjects=0; 
+    }
+    objects=0;    
+    count=0;
 }
 
 
 bool mtrk_objects::prepare(cJSON* section)
 {
-    // TODO
-    objects=section;
+    MTRK_LOG("Preparing Objects")
+    clear();
 
+    cJSON* entry=0;
+    cJSON_ArrayForEach(entry, section)
+    {
+        count++;
+    }        
+    
+    eventObjects=new mtrk_object*[count];
+    for (int i=0; i<count; i++)
+    {
+        eventObjects[i]=new mtrk_object();
+    }
+
+    int index=0;
+
+    entry=0;
+    cJSON_ArrayForEach(entry, section)
+    {
+        eventObjects[index]->prepare(entry);       
+        cJSON_AddNumberToObject(entry, MTRK_PROPERTIES_MEMINDEX, index);
+        index++;
+    }    
+
+    MTRK_LOG("Objects: " << count)
+    objects=section;
     return true;
+}
+
+
+
+
+mtrk_object::mtrk_object()
+{
+    type=INVALID;
+    duration=0;
+
+    eventGrad=0;
+    eventSync=0;
+    eventADC=0;
+    eventRF=0;
+    eventNCOSet=0;
+    eventNCOReset=0;
+
+    object=0;
+}
+
+
+mtrk_object::~mtrk_object()
+{
+    clear();
+}
+
+
+void mtrk_object::clear()
+{
+    MTRK_LOG("Object destructor")
+
+    MTRK_DELETE(eventGrad)
+    MTRK_DELETE(eventSync)
+    MTRK_DELETE(eventADC)
+    MTRK_DELETE(eventRF)
+    MTRK_DELETE(eventNCOSet)
+    MTRK_DELETE(eventNCOReset)
+
+    object=0;
+    type=INVALID;
+    duration=0;    
+}
+
+
+bool mtrk_object::prepare(cJSON* entry)
+{
+    MTRK_GETITEM(entry, MTRK_PROPERTIES_TYPE, objectType)
+
+    if (strcmp(objectType->valuestring, MTRK_ACTIONS_RF)==0)
+    {    
+        MTRK_LOG("Preparing RF")
+        type=RF;
+        eventRF=new sRF_PULSE_ARB();
+        eventRF->setIdent(entry->string);
+    }
+
+    object=entry;
 }
 
--- mtrk_objects.h
+++ mtrk_objects.h
@@ -3,9 +3,52 @@
 
 #include "cJSON.h"
 
+#include "MrServers/MrMeasSrv/SeqIF/libRT/libRT.h"
+#include "MrServers/MrMeasSrv/SeqIF/libRT/sREADOUT.h"
+#include "MrServers/MrMeasSrv/SeqIF/libRT/sRF_PULSE.h"
+#include "MrServers/MrMeasSrv/SeqIF/libRT/sGRAD_PULSE.h"
+#include "MrServers/MrMeasSrv/SeqIF/libRT/sSYNC.h"
+#include "MrServers/MrMeasSrv/SeqIF/libRT/sFREQ_PHASE.h"
+#include "MrServers/MrMeasSrv/MeasUtils/NLSStatus.h"
+
+
 namespace SEQ_NAMESPACE
 {  
     class mtrk_state;
+
+    class mtrk_object
+    {
+    public:
+        enum {
+            INVALID=-1,
+            GRADIENT=0,
+            RF=1,
+            ADC=2,
+            SYNC=3
+        };
+
+        mtrk_object();
+        virtual ~mtrk_object();
+        void clear();
+
+        bool prepare(cJSON* entry);
+
+        int type;
+        int duration;
+
+        sGRAD_PULSE_ARB* eventGrad;
+        sSYNC*           eventSync;
+        sREADOUT*        eventADC;
+        sRF_PULSE_ARB*   eventRF;
+        sFREQ_PHASE*     eventNCOSet;
+        sFREQ_PHASE*     eventNCOReset;
+
+        cJSON* object;
+
+        //bool insert(cJSON* item);
+        //int  getDuration(bool includeOverhang);    
+    };
+
 
     class mtrk_objects
     {    
@@ -16,11 +59,10 @@ namespace SEQ_NAMESPACE
         void clear();
         bool prepare(cJSON* section);
 
+        mtrk_object** eventObjects;
         cJSON* objects;
         int count;
     };
-
-
 
 }
 
